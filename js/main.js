ae58f660aee10adb6c12364878bc72de2df56d7c
alberto <albertozanola1996@gmail.com>
2021-07-18 23:27:07 +0200

    Inserimento email

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 var email = ["antonio@gmail.com", "tommy@gmail.com" , "anna@libero.it", "stefano@gmail.com"];
 var emailInserita = prompt("inserisci la tua email");
 
+for (var i = 0; i < email.length; i++) {
+    if (emailInserita == email[i]) {
+        console.log("accesso consentito");
+    }
+    else if (emailInserita != email[i]) {
+        console.log("accesso negato");
+    }
+}
+
+
+
+
+
 
